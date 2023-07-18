6412e7c6025532bced0f5a99b7728fc004d53ad4
Sonali553 <83964942+Sonali553@users.noreply.github.com>
2023-07-18 22:06:09 +0530

    Update matrix_Transpose.py

--- matrix_Transpose.py
+++ matrix_Transpose.py
@@ -1,18 +1,1 @@
-r, c = map(int, input().split())
-lst = []
-for i in range(0, r):
-    l = list(map(int, input().split()))
-    lst.append(l)
-def matrix_Transpose(lst):
-    r = len(lst)
-    c = len(lst[0])
-    result = []
-    for i in range(0, c):
-        res =[]
-        for j in range(0, r):
-            res.append(lst[j][i])
-        result.append(res)
-    return result
-print(matrix_Transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-print(matrix_Transpose([[1, 2],[1, 2], [1, 2]]))
-print(matrix_Transpose(lst))
+
